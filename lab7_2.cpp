99087fb5694ebd3aaf424859bc4b862460cb5d98
ZZMolotov <zzmolotov8559@gmail.com>
2025-12-12 16:32:20 +0700

    lab7_2 fixed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,42 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include <iostream>
+#include <string>  
+using namespace std;
+
+int GetFistTwoDigit(int n) {
+    while (n >= 100) {
+        n /= 10;
+    }
+    return n;
+}
+
+int main(){
+    string name;
+    int id;
+    string date;
+    string movie;
+    string random;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?"<<endl;
+    cout << "?????: ";
+    getline(cin, name);
+    cout << "Fahsai: Wow!!! "<<name<<" is a really cool name."<<endl;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?"<<endl;
+    cout <<name<< ": ";
+    cin  >> id;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR " << GetFistTwoDigit(id) -12<< ". I have a free movie ticket for you."<< endl;
+    cout << "Fahsai: Let's go to the cinema together!!!"<<endl;
+    cout << "Fahsai: What movie do you want to watch?"<<endl;
+    cout << name << ": ";
+
+    getline(cin,movie);
+    cout << "Fahsai: So....which day are you free to go with me?"<<endl;
+    cout << name << ": ";
+    getline(cin, date);
+    cout << "Fahsai: "<< date <<"....that is OK!!! I'm looking forward to watching "<< movie <<" with you."<<endl;
+    cout << name << ": ";
+    cin  >> random;
+    cout << "Fahsai: 555+ see you "<< date <<". Bye Bye \\(^ ^)/";
+}
